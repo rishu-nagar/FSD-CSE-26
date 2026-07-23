c644dd8bcaff634f0e6b291dbe8763cd7cc100f2
Rishu <rishavn32@gmail.com>
2026-07-23 10:07:55 +0530

    created ex1.js

---
+++ ex1.js
@@ -0,0 +1,16 @@
+var a=10;
+var b=20;
+console.log("a=",a);
+
+
+    let a=10;
+    let a= 20;
+    console.log(a);
+
+    let a=10;
+    let b= 20;
+    const c=30;
+    console.log(a);
+    console.log(b);
+    c=b*9;
+    console.log(c);
